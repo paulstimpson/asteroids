1ed1e83057ed4b4121c155c53d4df3fb65294906
paulstimpson <paul.stimpson@redfoxdigital.com>
2025-02-14 01:36:36 +0000

    Changed import syntax

--- main.py
+++ main.py
@@ -2,14 +2,14 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-import constants
+from constants import *
 
 def main():
     print("Starting asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
-    screen=pygame.display.set_mode((constants.SCREEN_WIDTH,constants.SCREEN_HEIGHT))
+    screen=pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 
     while True:
         for event in pygame.event.get():
